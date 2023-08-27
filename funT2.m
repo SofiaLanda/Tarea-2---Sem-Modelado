d0300c276bbb48db0a056d36ef642118b2e69d0d
Sofia Landa <Sofia.Landa@toshibagcs.com>
2023-08-27 17:32:52 -0600

    Proyecto agregado

---
+++ funT2.m
@@ -0,0 +1,17 @@
+function dx=funT2(t,x)
+%% PARAMETROS
+m1=0.2;
+L1=0.3;
+m2=0.1;
+L2=0.25;
+g=9.81;
+c=0.2;
+%% VECTORES/MATRICES
+dx=zeros(4,1);
+M=[(m1+m2)*L1 m2*L2*cos(x(2)-x(1)); L1*cos(x(2)-x(1)) L2];
+C=[c -m2*L2*x(4)*sin(x(2)-x(1)); L1*x(3)*sin(x(2)-x(1)) 0];
+G=[(m1+m2)*g*sin(x(1)); g*sin(x(2))];
+%% DINAMICA DEL SISTEMA
+dx(1)=x(3);
+dx(2)=x(4);
+dx(3:4)=M\(-C*[x(3); x(4)]-G);
---
+++ t02sdM.m
@@ -0,0 +1,13 @@
+[t,x]=ode45(@funT2,[0 10],[0 (35*pi)/180 0 0]);
+%%
+figure(1)
+plot(t,x(:,1));
+grid on
+title("theta 1");
+xlabel("tiempo");
+%%
+figure(2)
+plot(t,x(:,2));
+grid on
+title("theta 2")
+xlabel("tiempo");
